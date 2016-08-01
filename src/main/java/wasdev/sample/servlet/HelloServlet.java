6de1de495fc949f4bfdc79c30c1540ee0ed4ec5c
phongbm <phongbm.it@gmail.com>
2016-08-02 00:15:32 +0700

    config servlet

--- src/main/java/wasdev/sample/servlet/HelloServlet.java
+++ src/main/java/wasdev/sample/servlet/HelloServlet.java
@@ -1,7 +1,20 @@
 package wasdev.sample.servlet;
 
-/**
- * Created by PhongBM on 01/08/2016.
- */
-public class HelloServlet {
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(urlPatterns = "/hello")
+public class HelloServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        RequestDispatcher rs = req.getRequestDispatcher("hello.html");
+        rs.include(req, resp);
+    }
+
 }
--- src/main/java/wasdev/sample/servlet/HomeServlet.java
+++ src/main/java/wasdev/sample/servlet/HomeServlet.java
@@ -1,7 +1,20 @@
 package wasdev.sample.servlet;
 
-/**
- * Created by PhongBM on 01/08/2016.
- */
-public class HomeServlet {
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(urlPatterns = "/home")
+public class HomeServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        RequestDispatcher rs = req.getRequestDispatcher("home.html");
+        rs.include(req, resp);
+    }
+
 }
